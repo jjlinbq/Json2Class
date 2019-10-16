cc65ecb57619f60fde3cdd0d723da439be3e75bb
jjlin <1405999864@qq.com>
2019-10-16 10:59:07 +0800

    version 1.0

---
+++ Json2Class/Json2Class/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace JsonFomatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "转换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 382);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(444, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(344, 382);
+            this.textBox2.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(347, 173);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Json特性";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(347, 134);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "忽略空值";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(347, 59);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(84, 16);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Pascal命名";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(347, 98);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(78, 16);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "Camel命名";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ Json2Class/Json2Class/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonFomatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            checkBox3.Checked = true;
+            checkBox1.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var originalValue = textBox1.Text;
+            string str = "public class RootObject\n{\r\n";
+
+            var ret = GetClassLevel(str, originalValue, 0, true);
+            ret.Sort();
+            textBox2.Text = "";
+            for (var i = 0; i < ret.Count; i++)
+            {
+                textBox2.Text += "\r\n" + ret[i].Item2;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="NewStr">递归str</param>
+        /// <param name="orgStr">json对象</param>
+        /// <param name="count">递归层数</param>
+        /// <param name="isFirstLevel">是否是第一层</param>
+        /// <returns></returns>
+        private List<Tuple<int, string>> GetClassLevel(string NewStr, string orgStr, int count, bool isFirstLevel)
+        {
+            List<Tuple<int, string>> orgdic = new List<Tuple<int, string>>();
+            try
+            {
+                var obj = JsonConvert.DeserializeObject<JObject>(orgStr);
+                foreach (var item in obj)
+                {
+                    var key = item.Key;
+                    var value = item.Value;
+                    if (value.HasValues)//为对象
+                    {
+                        if (value.Type is JTokenType.Array)//数组
+                        {
+                            if (value.LastOrDefault().Type is JTokenType.Integer)
+                            {
+                                NewStr += GetJsonProperty(key);
+                                NewStr += "\tpublic List<int> " + GetProperName(key) + " {get;set;}\r\n";
+                            }
+                            else if (value.LastOrDefault().Type is JTokenType.String)
+                            {
+                                NewStr += GetJsonProperty(key);
+                                NewStr += "\tpublic List<string> " + GetProperName(key) + " {get;set;}\r\n";
+                            }
+                        }
+                        else if (value.Type is JTokenType.Object)
+                        {
+                            //递归
+                            NewStr += GetJsonProperty(key);
+                            NewStr += "\tpublic " + GetProperName(key) + " " + key.ToLower() + " {get;set;}\r\n";//类的属性名采用小写
+                            var ret = GetClassLevel("public class " + GetProperName(key) + " \n{\r\n", JsonConvert.SerializeObject(value), ++count, false);
+                            foreach (var val in ret)
+                            {
+                                orgdic.Add(Tuple.Create(val.Item1, val.Item2));
+                            }
+                            // count = 0;
+                        }
+                    }
+                    else//为值
+                    {
+                        if (value.Type is JTokenType.String)
+                        {
+                            NewStr += GetJsonProperty(key);
+                            NewStr += "\tpublic string " + GetProperName(key) + " {get;set;}\r\n";
+                        }
+                        else if (value.Type is JTokenType.Integer)
+                        {
+                            NewStr += GetJsonProperty(key);
+                            NewStr += "\tpublic int " + GetProperName(key) + " {get;set;}\r\n";
+                        }
+                        else if (value.Type is JTokenType.Date)
+                        {
+                            NewStr += GetJsonProperty(key);
+                            NewStr += "\tpublic DateTime " + GetProperName(key) + " {get;set;}\r\n";
+                        }
+                        else if (value.Type is JTokenType.Guid)
+                        {
+                            NewStr += GetJsonProperty(key);
+                            NewStr += "\tpublic Guid " + GetProperName(key) + " {get;set;}\r\n";
+                        }
+                        else if (value.Type is JTokenType.Boolean)
+                        {
+                            NewStr += GetJsonProperty(key);
+                            NewStr += "\tpublic bool " + GetProperName(key) + " {get;set;}\r\n";
+                        }
+                    }
+                }
+                NewStr += "}\r\n";
+                if (isFirstLevel)
+                {
+                    orgdic.Add(Tuple.Create(0, NewStr));
+                }
+                else
+                {
+                    orgdic.Add(Tuple.Create(count, NewStr));
+                }
+            }
+            catch (Exception e)
+            {
+                textBox2.Text = e.Message;
+            }
+            return orgdic;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetProperName(string key)
+        {
+            string ProperName = key;
+
+            if (checkBox3.Checked)//Pascal
+            {
+                ProperName = "";
+                if (key.IndexOf("_") > 0)//下划线命名
+                {
+                    var NameStr = key.Split('_');
+                    for (var i = 0; i < NameStr.Length; i++)
+                    {
+                        ProperName += Char.ToUpper(NameStr[i][0]) + NameStr[i].Substring(1);
+                    }
+                }
+                else//驼峰式
+                {
+                    ProperName += Char.ToUpper(key[0]) + key.Substring(1);
+                }
+            }
+            if (checkBox4.Checked)//Camel
+            {
+                ProperName = "";
+                if (key.IndexOf("_") > 0)//下划线命名
+                {
+                    var NameStr = key.Split('_');
+                    for (var i = 0; i < NameStr.Length; i++)
+                    {
+                        if (i!=0)
+                        {
+                            ProperName += Char.ToUpper(NameStr[i][0]) + NameStr[i].Substring(1);
+                        }
+                        else
+                        {
+                            ProperName += Char.ToLower(NameStr[i][0]) + NameStr[i].Substring(1);
+                        }
+                    }
+                }
+                else//驼峰式
+                {
+                    ProperName += Char.ToLower(key[0]) + key.Substring(1);
+                }
+            }
+            return ProperName;
+        }
+        private string GetJsonProperty(string key)
+        {
+            string propertystr = string.Empty;
+            bool flag = false;
+            if (checkBox1.Checked)
+            {
+                flag = true;
+                propertystr += "\"" + key + "\",";
+            }
+            if (checkBox2.Checked)
+            {
+                flag = true;
+                propertystr += "NullValueHandling=NullValueHandling.Ignore,";
+            }
+            if (flag)
+            {
+                propertystr = "\t[JsonProperty(" + propertystr.TrimEnd(',') + ")]\r\n";
+            }
+            return propertystr;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                checkBox4.Checked = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
